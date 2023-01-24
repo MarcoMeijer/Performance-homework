cd4ac9d2ddbf941412fed2bbf83b7bda733541d0
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/ItemBox.tsx
@@ -0,0 +1,19 @@
+import { IoIosClose } from "react-icons/io";
+import { Item } from "./getRandomItem";
+
+interface ItemBoxProps {
+  item: Item;
+  onRemove: () => void;
+}
+
+export const ItemBox = ({ item, onRemove }: ItemBoxProps) => {
+  console.log("item");
+  return (
+    <div style={{ background: item.background }}>
+      <h3>{item.word}</h3>
+      <button onClick={onRemove}>
+        <IoIosClose onClick={onRemove} className="icon-remove" size="2.5em" />
+      </button>
+    </div>
+  );
+};
